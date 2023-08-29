8112b71294933894910fbe253c8ede75963aabe2
MdSameer26 <sameer26md@gmail.com>
2023-08-29 15:16:14 +0530

    second

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to DIET Collage..." );
+        System.out.println( "Welcome to DIET Collage,This is WorkShop Session ..." );
     }
 }
